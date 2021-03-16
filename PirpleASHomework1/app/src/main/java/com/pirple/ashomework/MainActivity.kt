962822524ebd6f0e53d78d6cd6a975e15d6fb21c
ThribsClube <thiago@clubedochaveiro.com.br>
2021-03-16 09:36:52 -0300

    Released for submission

---
+++ PirpleASHomework1/app/src/main/java/com/pirple/ashomework/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.pirple.ashomework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.text_view)
+    }
+
+    fun buttonPress(view: View) {
+        textView.text = getString(R.string.hello)
+    }
+}
